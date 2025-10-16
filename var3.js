9b26134364c30d341f43b982269cd29975653132
Mehrinwagle <mehrinwagle26@gmail.com>
2025-10-16 15:41:28 +0530

    file added

---
+++ var3.js
@@ -0,0 +1,61 @@
+//string
+let y='hello world';
+console.log(y)
+console.log(typeof y)
+
+//number
+let z=123;
+console.log(z)
+console.log(typeof z)
+
+//BigInt
+let d=12345678901113n;
+console.log(d)
+console.log(typeof d)
+
+let a=BigInt('1234567890112');
+console.log(a)
+console.log(typeof a)
+
+//Boolean
+let s=true;
+console.log(s)
+console.log(typeof s)
+
+let f=false;
+console.log(f)
+console.log(typeof f)
+
+//object
+const person = {
+    name : "fatima",
+    surname : "bandri",
+}
+console.log(person)
+console.log(typeof person)
+
+//array object
+const car = ["BMW","bajaj","tvs"];
+console.log(car)
+console.log(typeof car)
+
+//date object
+const date = new Date('2025-10-15');
+console.log(date)
+console.log(typeof date)
+
+//undefined
+let i;
+console.log(i)
+console.log(typeof i)
+
+//null
+let j=null;
+console.log(null)
+console.log(typeof null)
+
+//symbol
+const symbol =Symbol();
+console.log(symbol)
+console.log(typeof symbol)
+
